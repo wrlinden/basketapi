b02403e0baf190bb468a8cb08487cca5de902b3a
William Linden <william.linden@just-eat.com>
2019-01-27 16:27:46 +0000

    Introduce Basket model and contracts.

---
+++ BasketApi.Contracts/BasketContract.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace BasketApi.Contracts
+{
+    public class BasketContract
+    {
+        public int Id { get; set; }
+
+        public IList<BasketContractItem> Items { get; set; }
+
+    }
+
+    
+    public class BasketContractItem
+    {
+        public int Id { get; set; }
+        public ContractItem ContractItem { get; set; }
+        public int Qty { get; set; }
+
+
+    }
+
+    public class ContractItem
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ BasketApi.Web/Models/BasketModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace BasketApi.Models
+{
+    public class BasketModel
+    {
+        public int Id { get; set; }
+
+        public IList<BasketModelItem> Items { get; set; }
+    }
+
+    public class BasketModelItem
+    {
+        public int Id { get; set; }
+        public ModelItem ModelItem { get; set; }
+        public int Qty { get; set; }
+
+
+    }
+
+    public class ModelItem
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
